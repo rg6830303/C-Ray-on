b42a034b0ad5b881f78299f61760868e2ba411fb
ThestralWarrior <ashbroezio@outlook.com>
2024-08-24 13:44:04 +0530

    updated main

--- src/main.c
+++ src/main.c
@@ -10,10 +10,20 @@ int main() {
     int width = 800;
     int height = 800;
     Camera camera = {{0, 0, 1}, {0, 0, -1}};
-    Sphere sphere = {{0, 0, -5}, 1}; 
-    Vector3 light_direction = {1.0f, -1.0f, -1.0f};
+    Sphere sphere1 = {{0, 0, -5}, 1};
+    Sphere sphere2 = {{-3, -1, -5}, 1};
+    Sphere sphere3 = {{3, 1, -5}, 1};
+    Scene scene = (Scene){
+        (Sphere[]){sphere1, sphere2, sphere3}
+    };
+    Vector3 light_direction = {0.0f, 0.0f, 1.0f};
     Color sphere_color = {1.0f, 0.0f, 0.0f};
-    render(width, height, camera, sphere, light_direction, sphere_color);
+    printf("Listing the contents of the scene.\n");
+    for(int i = 0; i < 3; i++) {
+        printf("(Sphere) Center: %f %f %f, Radius: %f\n", scene.spheres[i].center.x, scene.spheres[i].center.y, scene.spheres[i].center.z, scene.spheres[i].radius);
+    }
+    printf("Starting rendering...\n");
+    render(width, height, camera, scene, 3, light_direction, sphere_color);
     printf("Rendering complete. Image saved as result.ppm.\n");
     return 0;
 }
