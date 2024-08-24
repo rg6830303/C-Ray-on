62987df42e99b4e2186bbd9df42663fac64472ff
ThestralWarrior <ashbroezio@outlook.com>
2024-08-24 14:37:24 +0530

    main changes

--- src/main.c
+++ src/main.c
@@ -7,17 +7,17 @@
 #include "rendering.h"
 
 int main() {
-    int width = 800;
-    int height = 800;
+    int width = 400;
+    int height = 400;
     Camera camera = {{0, 0, 1}, {0, 0, -1}};
-    Sphere sphere1 = {{0, 0, -5}, 1};
-    Sphere sphere2 = {{-3, -1, -5}, 1};
-    Sphere sphere3 = {{3, 1, -5}, 1};
+    Sphere sphere1 = {{0, 0, -10.0f}, 1};
+    Sphere sphere2 = {{-3, 1, -6.0f}, 1};
+    Sphere sphere3 = {{1, -1, -4.0f}, 1};
     Scene scene = (Scene){
         (Sphere[]){sphere1, sphere2, sphere3}
     };
-    Vector3 light_direction = {0.0f, 0.0f, 1.0f};
-    Color sphere_color = {1.0f, 0.0f, 0.0f};
+    Vector3 light_direction = {-1.0f, 0.0f, 0.0f};
+    Color sphere_color = {0.0f, 1.0f, 0.0f};
     printf("Listing the contents of the scene.\n");
     for(int i = 0; i < 3; i++) {
         printf("(Sphere) Center: %f %f %f, Radius: %f\n", scene.spheres[i].center.x, scene.spheres[i].center.y, scene.spheres[i].center.z, scene.spheres[i].radius);
